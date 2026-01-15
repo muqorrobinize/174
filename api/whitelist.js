33ebf5bc7e794db4ff7807c5d91bf9495d9d6d1c
B$4BE!N <122424313+muqorrobinize@users.noreply.github.com>
2026-01-15 20:32:11 +0700

    Create whitelist.js with predefined addresses
    Added a whitelist of Solana addresses for access control.

---
+++ api/whitelist.js
@@ -0,0 +1,16 @@
+export const whitelist = [
+    // --- TIM INTI ---
+    "0x918dfeea08756ccb0bd045108883d8f89013f215".toLowerCase(), 
+    "YourSolanaAddressHere",
+
+    // --- KELAS A ---
+    "0x1234567890abcdef1234567890abcdef12345678",
+    "0xabcdef1234567890abcdef1234567890abcdef12",
+    "SolanaAddressMember1........................",
+    
+    // --- KELAS B ---
+    "0x9876543210fedcba9876543210fedcba98765432",
+    "SolanaAddressMember2........................",
+
+    // ... tambahkan ribuan wallet di sini ...
+];
